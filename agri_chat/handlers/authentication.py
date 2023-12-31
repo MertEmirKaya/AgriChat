76770a0b03d2f39de8994b24ea5587085fac7b4c
MertEmirKaya <mertemir_54@outlook.com>
2023-12-31 22:19:22 +0300

    wrote the register function

--- agri_chat/handlers/authentication.py
+++ agri_chat/handlers/authentication.py
@@ -1,8 +1,30 @@
 """The python module for the authentication handler functions."""
 
-
+import json
+import uuid
 from typing import Any
 
+import bcrypt
+from models.user import User
+from utils.utils import status_json
 
 
-def login(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
+def register(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
+    """Register a new user."""
+    body = json.loads(event["body"])
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(body["password"].encode("utf-8"), salt)
+    try:
+        user = User(
+            hash_key=str(uuid.uuid4()),
+            range_key=body["email"],
+            first_name=body["first_name"],
+            last_name=body["last_name"],
+            hashed_password=hashed_password.decode("utf-8"),
+            salt=salt.decode("utf-8")
+        )
+        user.save()
+    except Exception as e:
+        return status_json(500, {"message": str(e)})
+
+    return status_json(200, {"message": "User created successfully."})
