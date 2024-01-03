bb39a225ed386d811843c17a1b43619c0ae6a8e1
MertEmirKaya <mertemir_54@outlook.com>
2024-01-03 22:16:16 +0300

    added the payload to the jwt encode

--- agri_chat/handlers/authentication.py
+++ agri_chat/handlers/authentication.py
@@ -1,9 +1,12 @@
 """The python module for the authentication handler functions."""
 
+import datetime
 import json
+import os
 from typing import Any
 
 import bcrypt
+import jwt
 from models.user import User
 from utils.utils import status_json
 
@@ -37,7 +40,17 @@ def login(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
     entered_password = body.get("password", "").encode("utf-8")
     hashed_password = bcrypt.hashpw(entered_password, user.salt.encode("utf-8")).decode("utf-8")
     if hashed_password == user.hashed_password:
-        return status_json({"message": "User logged in successfully."}, 200)
+        expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+
+        payload = {
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "exp": expiration_time,
+        }
+
+        token = jwt.encode(payload, os.environ["SECRET_KEY"], algorithm="HS256")
+        return status_json({"token": token}, 200)
 
     else:
         return status_json({"message": "Either your email or password is incorrect"}, 401)
