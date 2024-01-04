78577a423e2a88897847b33d9f9091c8ff6befd6
MertEmirKaya <mertemir_54@outlook.com>
2024-01-04 22:40:37 +0300

    user email now being reached from token's payload

--- agri_chat/handlers/forms.py
+++ agri_chat/handlers/forms.py
@@ -5,23 +5,20 @@ import uuid
 from typing import Any
 
 from models.form import Form
+from utils.utils import get_token_data, status_json
 
 
 def create_form(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
     """Create a new form."""
     body = json.loads(event["body"])
-
+    token_data = get_token_data(event["headers"]["Authorization"])
     form = Form(
         hash_key=uuid.uuid4().hex,
-        user_email=body["user_email"],
+        user_email=token_data["email"],
         title=body["title"],
         text=body["text"]
     )
     form.save()
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Form created successfully."
-        })
-    }
+    return status_json({"message": "Form created successfully."}, 200)
+
