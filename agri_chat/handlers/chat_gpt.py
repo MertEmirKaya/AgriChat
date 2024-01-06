c11e4e0bf072a18fb3f396dab7a877d0721ece47
MertEmirKaya <mertemir_54@outlook.com>
2024-01-06 14:02:56 +0300

    user and bot messages are being recorded now

--- agri_chat/handlers/chat_gpt.py
+++ agri_chat/handlers/chat_gpt.py
@@ -3,14 +3,21 @@
 import os
 from typing import Any
 
+from models.message import Message
 from openai import OpenAI
-from utils.utils import status_json
+from utils.utils import get_token_data, status_json
 
 
 def chat_gpt(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
     """Get the message from the query string and return the response from GPT-3."""
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     content = event["queryStringParameters"].get("message")
+    if not content:
+        return status_json({"message": "No message provided", "status": 400})
+
+    token_data = get_token_data(event["headers"]["Authorization"])
+    message = Message(owner=token_data["email"], text=content)
+    message.save()
     completion = client.chat.completions.create(
         messages=[
             {
@@ -20,4 +27,7 @@ def chat_gpt(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
         ],
         model="gpt-3.5-turbo-1106"
     )
-    return status_json({"message": completion.choices[0].message.content, "status": "success"})
+    bot_message = Message(owner="bot", text=completion.choices[0].message.content)
+    bot_message.save()
+
+    return status_json({"message": completion.choices[0].message.content, "status": 200})
