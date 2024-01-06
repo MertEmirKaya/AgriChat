8523a159c07f915be73d65eaab8f0f10efc6d6ee
MertEmirKaya <mertemir_54@outlook.com>
2024-01-06 14:02:18 +0300

    created the Form model for forms table

--- agri_chat/models/message.py
+++ agri_chat/models/message.py
@@ -1,22 +1,21 @@
-"""The form model."""
-import uuid
+"""The message model for chat-gpt messages."""
+
 from datetime import datetime
 
 from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 from pynamodb.models import Model
 
 
-class Form(Model):
-    """The form model."""
+class Message(Model):
+    """The message model."""
 
     class Meta:
         """The meta class."""
 
-        table_name = "Forms"
+        table_name = "Messages"
         region = "eu-west-1"
 
-    id = UnicodeAttribute(hash_key=True, default=uuid.uuid4().hex)
-    user_email = UnicodeAttribute()
-    title = UnicodeAttribute()
+    id = UnicodeAttribute(hash_key=True)
+    owner = UnicodeAttribute(range_key=True) # either user email or "bot"
     text = UnicodeAttribute()
     created_at = UTCDateTimeAttribute(default=datetime.now)
