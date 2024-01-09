38811faeb4c6effca10f5bbb6607d4dcd7d20553
MertEmirKaya <mertemir_54@outlook.com>
2024-01-09 21:54:27 +0300

    added the get history gpt api

--- agri_chat/handlers/chat_gpt.py
+++ agri_chat/handlers/chat_gpt.py
@@ -27,7 +27,14 @@ def chat_gpt(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
         ],
         model="gpt-3.5-turbo-1106"
     )
-    bot_message = Message(owner="bot", text=completion.choices[0].message.content)
+    bot_message = Message(owner=token_data["email"], text=completion.choices[0].message.content, bot=True)
     bot_message.save()
 
     return status_json({"message": completion.choices[0].message.content, "status": 200})
+
+
+def get_history_gpt(event:dict[str, Any], context:dict[str, Any]) -> dict[str, Any]:
+    """Get the message history for the user."""
+    token_data = get_token_data(event["headers"]["Authorization"])
+    messages = Message.query(token_data["email"])
+    return status_json({"messages": [message.text for message in messages], "status": 200})
